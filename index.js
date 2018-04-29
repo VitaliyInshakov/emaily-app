80543e4d4a84c8e29d77fca4e62ef1664d49df60
Vitalii Inshakov <michael8913@gmail.com>
2018-04-29 21:20:53 +0300

    some fixes in code

--- index.js
+++ index.js
@@ -14,9 +14,11 @@ app.use(cookieSession({
   maxAge: 30 * 24 * 60 * 60 * 1000,
   keys: [keys.cookieKey],
 }));
-require('./routes/authRoutes')(app);
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
